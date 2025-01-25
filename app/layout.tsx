ee1bf98a19cc0a8413c89fa4f8689e826cc7dd1e
José Vinícius <jvini_19@hotmail.com>
2025-01-25 15:40:50 -0300

    chore(config): :wrench: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,31 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import React from 'react';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: "CyberKarr Pokédex",
-  description: "Aplicação front-end para exibição de dados de Pokémon!",
+  title: 'CyberKarr Pokédex',
+  description: 'Aplicação front-end para exibição de dados de Pokémon!'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
